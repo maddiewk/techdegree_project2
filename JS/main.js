e7ad094ab824244aa78879c271a6b9ba03d5c80b
Madeline Watson <mcwatson91@gmail.com>
2018-04-06 12:38:19 -0400

    Begin function to show a list of ten students

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,18 @@
 //$('.student-list')
 
 // add a <div> element with class "pagination"
-const $pagination = $("body").add("div").addClass("pagination");
-$("body").append($pagination);
-
 // need variable containing total length of student list
 // use a for loop to loop through the student list. for every 10 students, create a new
 // button for the next page of students
+
+// create function to display list of 10 students
+
+function showPage(page, studentList) {
+  // hide all studens on the page
+    $('.student-list').hide();
+  // loop through all students in the student list argument
+    // if student should be on this page number
+      // show the student
+
+
+}
