7ff00a9fc1b19f35d3c3029366c7a700c5ba2a1c
Madeline Watson <mcwatson91@gmail.com>
2018-04-06 12:33:11 -0400

    attempting to dynamically create div element

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,10 @@
 // select unordered list
-$('.student-list').hide();
+//$('.student-list')
+
+// add a <div> element with class "pagination"
+const $pagination = $("body").add("div").addClass("pagination");
+$("body").append($pagination);
+
+// need variable containing total length of student list
+// use a for loop to loop through the student list. for every 10 students, create a new
+// button for the next page of students
