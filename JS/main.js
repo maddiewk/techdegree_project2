d052ee5b78101b0df92c0568bc24c379b172ab09
Madeline Watson <mcwatson91@gmail.com>
2018-04-06 14:24:02 -0400

    test

--- JS/main.js
+++ JS/main.js
@@ -2,8 +2,6 @@
 //$('.student-list')
 
 // add a <div> element with class "pagination"
-  const $pagination = $(".page").add("<div>").addClass("pagination");
-  $("body").append($pagination);
   // need variable containing total length of student list
 // use a for loop to loop through the student list. for every 10 students, create a new
 // button for the next page of students
