08f219ba25ed3b4f930826ee81bd61b4e17e62f7
Madeline Watson <mcwatson91@gmail.com>
2018-04-08 08:37:51 -0400

    add global variables

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,17 @@
-// select unordered list
-//$('.student-list')
-
 // add a <div> element with class "pagination"
   // need variable containing total length of student list
 // use a for loop to loop through the student list. for every 10 students, create a new
 // button for the next page of students
 
+// set up variables
+const eachStudent = $(".student-item");
+const pageNum = 0;
+const studentsPerPage = 10;
+
 // create function to display list of 10 students
 
 function showPage(pageNumber, studentList) {
-  // const allStudents = $(".student-list").children();
+
   // hide all students on the page
     $(".student-list").hide();
   // loop through all students in the student list argument
