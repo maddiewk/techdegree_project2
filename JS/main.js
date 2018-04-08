8ff2f26bc2f6a1be013b9f5de0216461cf7a7fe7
Madeline Watson <mcwatson91@gmail.com>
2018-04-08 10:06:18 -0400

    experimenting with adding page links

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,9 @@
 // set up variables
 const eachStudent = $(".student-item");
 const allStudents = $(".student-list");
-const pageNumber = 0;
+var pageNumber = 0;
 const studentsPerPage = 10;
-var totalPages = Math.ceil(allStudents / 10);
+
 
 // create function to display list of 10 students
 
@@ -24,18 +24,23 @@ function showPage(pageNumber) {
       }
   });
 }
-showPage(6);
+showPage(1);
 
 function appendPageLinks() {
   // determine how many pages for this student list
+  var totalPages = Math.ceil(allStudents / 10);
     // create a page link section
+    const div = $(".student-list").add("<div class='pagination'><ul><li></li></ul></div>");
+    allStudents.append(div);
     // “for” every page
+    for ( pageNumber; pageNumber <= allStudents.length; pageNumber += 1 ) {
+      $(".pagination").add("<a href='#'></a>")
+    }
         // add a page link to the page link section
     // remove the old page link section from the site
     // append our new page link section to the site
     // define what happens when you click a link
         // Use the showPage function to display the page for the link clicked
         // mark that link as “active”
-
-
 }
+appendPageLinks();
