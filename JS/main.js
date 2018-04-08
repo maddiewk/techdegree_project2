3de11a88af664a3453a0f47b8b2093999a422fc3
Madeline Watson <mcwatson91@gmail.com>
2018-04-08 19:25:14 -0400

    added/edited comments for code

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,28 @@
-// set up variables
+  // set up variables for showPage function
 const eachStudent = $(".student-item");
 const allStudents = eachStudent.length;
 var pageNumber = 0;
 const studentsPerPage = 10;
 
-
-// create function to display list of 10 students
-
+  // create showPage function to display 10 students
 function showPage(pageNumber) {
-  // hide all students on the page
+  // first hide all students on the page
     eachStudent.hide();
-  // loop through all students in the student list argument
+  // loop through all student items using the each() method and only display 10 at a time
   eachStudent.each(function(index) {
-    // if student should be on this page number
-      // show the student
       if (index >= studentsPerPage * (pageNumber - 1) && index < studentsPerPage * pageNumber) {
         $(this).show();
       }
   });
 }
 
-function appendPageLinks(studentList) {
+  /* create appendPageLinks function to create page links, then  append those page links dynamically
+  to the page */
+function appendPageLinks() {
   // determine how many pages for this student list
   var totalPages = Math.ceil(allStudents / 10);
-    // create a page link section
+  // create a new page link section at the bottom of page
     $(".page").append("<div class='pagination'><ul></ul></div>");
-    // “for” every page
     for ( pageNumber; pageNumber < totalPages; pageNumber += 1 ) {
       if(pageNumber == 0) {
         $(".pagination").append("<li><a class='active' href='#'>" + (pageNumber + 1) + "</a></li>");
@@ -33,15 +30,13 @@ function appendPageLinks(studentList) {
         $(".pagination").append("<li><a href='#'>" + (pageNumber + 1) + "</a></li>");
       }
     }
+  // display first page of 10 students
       showPage(1);
+  // when different links are clicked, the click function displays the current link button as "active"
     $(".pagination a").click(function() {
       $(".pagination a").removeClass("active");
       $(this).addClass("active");
       showPage(parseInt($(this).text()));
     });
-
-    // define what happens when you click a link
-        // Use the showPage function to display the page for the link clicked
-        // mark that link as “active”
 }
 appendPageLinks();
